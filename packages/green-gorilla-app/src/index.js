2afa98099d181e63e907620eb6d7aeed2a7643f2
mattia richetto <mattia.richetto@gmail.com>
2017-07-15 01:26:00 +0100

    remove registerServiceWorker

--- packages/green-gorilla-app/src/index.js
+++ packages/green-gorilla-app/src/index.js
@@ -2,10 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import getData from './actions';
 import App from './containers/App/App';
-import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 const actions = { getData };
 
 ReactDOM.render(<App actions={actions} />, document.getElementById('root'));
-registerServiceWorker();
