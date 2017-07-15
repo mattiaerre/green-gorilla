5e197b57a4ff33e6ffe9c6bee3d332c9e511df71
mattia richetto <mattia.richetto@gmail.com>
2017-07-15 01:06:13 +0100

    update react-open-weather-map

--- packages/green-gorilla-app/src/actions/mock.js
+++ packages/green-gorilla-app/src/actions/mock.js
@@ -2,6 +2,15 @@ const mock = {
   info: {
     name: 'red-rhinoceros',
     version: '2.0.0-alpha.4'
+  },
+  currentWeather: {
+    name: 'London',
+    weather: [
+      { icon: '09d', description: 'shower rain' }
+    ],
+    sys: { country: 'GB' },
+    main: { temp: 23.33 },
+    dt: 1499694600
   }
 };
 
--- packages/green-gorilla-app/src/containers/App/App.js
+++ packages/green-gorilla-app/src/containers/App/App.js
@@ -7,8 +7,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      info: { name: '', version: '' },
-      currentWeather: { weather: [{}], sys: {}, main: {} }
+      info: { name: '', version: '' }
     };
   }
 
